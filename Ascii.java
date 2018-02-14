f3cb0ff80f19a4f7fe56b71f63e19cf53ff6c19d
bayquiz <36464751+bayquiz@users.noreply.github.com>
2018-02-13 21:25:33 -0600

    converting to ascii for wednesday class

---
+++ Ascii.java
@@ -0,0 +1,15 @@
+public class Ascii {
+    public static void main (String args []) {
+        char [] ascii = new char[128];
+        int index = 0;
+        //for(int =33; i <127;) {
+        for (char c='!'; c <= '~'; c++) {
+            ascii[index++] = c;
+        }
+        String result = new String(ascii);
+        System.out.println(result);
+       // System.out.println(ascii[45]);
+            //System.out.println(Character.toString((char)i));}
+        //System.out.println((char)i);
+    }
+}
